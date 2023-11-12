63be15251a6ce0a5c74d6a84633ea93129988af2
Mashedpotato98 <thiccbhai44@gmail.com>
2023-11-12 20:44:27 +0600

    yoooo

--- scripts/enemy/enemy_1/Enemy1_spotted.gd
+++ scripts/enemy/enemy_1/Enemy1_spotted.gd
@@ -5,20 +5,18 @@ var player = null
 
 @export var nav_agent:NavigationAgent3D
 @export var enemy:CharacterBody3D
-@export var player_path:NodePath
 
-var spd:int = 400
+var spd:int = 40
 var grav:int = 500
 
-func Enter():
-	player = get_node(player_path)
-
 func Physics_update(_delta:float):
-	enemy.velocity = Vector3.ZERO
-
-	nav_agent.set_target_position(player.global_transform.origin)
-	var next_nav_point = nav_agent.get_next_path_position()
-	enemy.velocity = (next_nav_point - enemy.global_transform.origin).normalized() * spd
+	if player != null:
+		enemy.position += (player.position - enemy.position) / spd
 
 	enemy.move_and_slide()
 
+func _on_detection_body_entered(body):
+	player = body
+
+func _on_detection_body_exited(body):
+	player = null
